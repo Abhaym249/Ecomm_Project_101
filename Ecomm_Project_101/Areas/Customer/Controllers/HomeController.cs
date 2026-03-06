3576c4196290a1813b14dfbe3e155806450dcacb
Abhaym249 <abhaym249@gmail.com>
2026-03-06 11:58:16 +0530

    added areas

--- Ecomm_Project_101/Areas/Customer/Controllers/HomeController.cs
+++ Ecomm_Project_101/Areas/Customer/Controllers/HomeController.cs
@@ -2,8 +2,9 @@ using System.Diagnostics;
 using Ecomm_Project_101.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Ecomm_Project_101.Controllers
+namespace Ecomm_Project_101.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         public IActionResult Index()
