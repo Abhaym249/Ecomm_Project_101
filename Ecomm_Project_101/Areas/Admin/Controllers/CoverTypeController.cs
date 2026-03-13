8ee69aa492263c45a528e8af6b3e7a6b2d016680
Abhaym249 <abhaym249@gmail.com>
2026-03-13 19:32:26 +0530

    added new controller for Covertype

---
+++ Ecomm_Project_101/Areas/Admin/Controllers/CoverTypeController.cs
@@ -0,0 +1,19 @@
+﻿using Ecomm_Project_101.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecomm_Project_101.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CoverTypeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CoverTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
